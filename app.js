ef1e19007bd1f7248f41e52ed1db4f047c93fb57
Hmanshu-Gurjar <himanshu.gurjar0013@gmail.com>
2022-08-04 15:44:16 +0530

    added dependencies and main app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express           = require('express');
+const config            = require('config');
+const startupService    = require('./services/startupservices');
+
+app = express();
+app.set('port', process.env.PORT || config.get('PORT'));
+app.set("view engine", "ejs");
+
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
+startupService.initializeServer();
